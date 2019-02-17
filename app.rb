63af39529f12c1f08367d9c8e8f8ff03db48b45c
pouchulm <evikyan24@gmail.com>
2019-02-17 20:21:07 +0900

    fixed bugs

--- app.rb
+++ app.rb
@@ -6,8 +6,11 @@ require 'open-uri'
 require 'nokogiri'
 
 get '/' do
-  @links = Array.new
-  @imgs = Hash.new
+  @supporter_links = Array.new
+  @supporter_imgs = Hash.new
+  @supporter_name = Hash.new
+  @school_links = Array.new
+  @school_name = Hash.new
   url = 'http://lit.gahaku.tech/old/'
   html = open(url).read
   parsend_html = Nokogiri::HTML.parse(html,nil,'utf-8')
@@ -15,10 +18,20 @@ get '/' do
   parsend_html.css('ul.supporter-list').css('li').each do |node|
     anchor = node.css('a')
     temp = anchor.css('img')
-    @links[i] = temp.attribute('alt').value
+    @supporter_links[i] = anchor.attribute('href').value.gsub(/https:\/\/web.archive.org\/web\/20170603211131\//,"")
+    @supporter_name[@supporter_links[i]] = temp.attribute('alt').value
     temp.attribute('src').value = url + temp.attribute('src').value
-    @imgs[@links[i]] = temp.to_html
+    @supporter_imgs[@supporter_links[i]] = temp.to_html
     i = i +1
+  end
+
+  j = 0
+  parsend_html.css('ul.school-list').css('li').each do |node|
+    anchor = node.css('a')
+    temp = anchor.css('h3')
+    @school_links[j] = anchor.attribute('href').value.gsub(/https:\/\/web.archive.org\/web\/20170603211131\//,"")
+    @school_name[@school_links[j]] = temp.text
+    j = j + 1
   end
   erb :index
 end
